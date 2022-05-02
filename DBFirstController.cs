87a3bc769bc41d5820a5108a5a7c8a1714af3fbc
RanjithChe55 <102515797+RanjithChe55@users.noreply.github.com>
2022-05-02 16:18:13 +0530

    Add files via upload
    Database First Approach

---
+++ DBFirstController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataBaseFirst.Models;
+using System.Data.SqlClient;
+
+namespace DataBaseFirst.Controllers
+{
+    public class DBFirstController : Controller
+    {
+        // GET: DBFirst
+        footballEntities context = new footballEntities();
+        public ActionResult Index()
+        {
+            List<footballdet> result=context.footballdets.Where(x=>x.Status=="Win").ToList();
+            return View(result);
+        }
+    }
+}
